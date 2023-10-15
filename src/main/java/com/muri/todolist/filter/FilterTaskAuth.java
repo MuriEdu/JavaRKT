38df5eb74fedf417115300b8aeb9e04e17e42df0
MuriEdu <muriloeduardo6@gmail.com>
2023-10-14 23:31:34 -0300

    Deploy

---
+++ src/main/java/com/muri/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,59 @@
+package com.muri.todolist.filter;
+
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.muri.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+        if(servletPath.startsWith("/tasks")){
+            //Buscar autenticação
+            var authorization = request.getHeader("Authorization");
+            var authEncoded = authorization.substring("Basic".length()).trim();
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecode);
+
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            //Validação Usuário
+            var user = this.userRepository.findByUsername(username);
+            if(user == null){
+                response.sendError(401);
+            } else {
+                //Validação Senha
+                var passwordVerifyRes = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if(passwordVerifyRes.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+            }
+        } else {filterChain.doFilter(request, response);}
+
+
+
+
+    }
+}
---
+++ src/main/java/com/muri/todolist/task/TaskController.java
@@ -0,0 +1,60 @@
+package com.muri.todolist.task;
+
+import com.muri.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || taskModel.getEndAt().isBefore(taskModel.getStartAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data Inválida");
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+        var task = this.taskRepository.findById(id).orElse(null);
+        var idUser = request.getAttribute("idUser");
+
+        if(task == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Task não encontrada");
+        }
+
+        if(!task.getIdUser().equals(idUser)){
+            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário Não Autorizado");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+        taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+}
